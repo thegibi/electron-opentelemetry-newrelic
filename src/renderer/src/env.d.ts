fbafc57218e08598f6168737a731ed821f2a34ce
gibify <gibifyofficial@gmail.com>
2024-06-20 20:50:08 -0300

    types

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly MAIN_VITE_SOME_KEY: string
+  readonly VITE_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: string
+  readonly VITE_OTEL_EXPORTER_OTLP_API_KEY: string
 }
 
 interface ImportMeta {
